3b8490a5df457da51d61e9b7ef1e061bb53ac48a
stevie2codes <stevie2codes@gmail.com>
2020-07-28 12:30:15 -0500

    started header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #212121;
+  padding: 20px;
+  color: #fefefe;
+}
+h1 {
+  margin: 0;
+}
+</style>
