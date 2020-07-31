e530cdc0332a26a9065ab3bf163b3a5088df1896
stevie2codes <stevie2codes@gmail.com>
2020-07-31 15:56:50 -0500

    movie search working and displaying movie

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,44 +1,63 @@
 <template>
-  <form @submit.prevent="processInput">
-    <input
-      type="text"
-      v-model="input"
-      name="input"
-      placeholder="Search a movie title"
-    />
-    <button type="submit">Enter</button>
-    <p>input is: {{ input }}</p>
-  </form>
+  <div>
+    <form @submit.prevent="processInput">
+      <input
+        type="text"
+        v-model="input"
+        name="input"
+        placeholder="Search a movie title"
+      />
+      <button type="submit">Enter</button>
+    </form>
+    <ul>
+      <li v-for="(movie, index) in movieResult" :key="index">
+        <Movie :movie="movie" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import Movie from "../components/Movie";
 export default {
   name: "Search",
+  result: false,
 
   data() {
     return {
-      input: ""
+      input: "",
+      movieResult: []
     };
   },
   methods: {
     processInput: async function() {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/search/movie?api_key=27a97db4259eddae7c5074e18978bd3c&language=en-US&query=${this.input}&page=1&include_adult=false`
+          `https://api.themoviedb.org/3/search/movie?api_key=27a97db4259eddae7c5074e18978bd3c&language=en-US&query=${this.input}&page=1&include_adult=true`
         );
         const movieResult = await res.json();
-        this.movieResult = movieResult;
+        this.movieResult = movieResult.results;
         console.log(movieResult);
       } catch (err) {
         console.log(err);
       }
     }
+  },
+  components: {
+    Movie
   }
 };
 </script>
 
 <style lang="scss" scoped>
-p {
-  color: white;
+ul {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+  justify-content: center;
+}
+li {
+  margin: 20px;
 }
 </style>
